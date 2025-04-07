f64006f4b3486e5d24d972ab7a6e4c325aed1bf2
Phoenix Gosper <59917842+FeenoPH@users.noreply.github.com>
2025-04-07 10:17:44 +1000

    Update product.py

--- Week10/product.py
+++ Week10/product.py
@@ -1,1 +1,14 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.__price = price
+        self.__quantity = quantity
 
+    def get_details(self):
+        print(f"Name: {self.name}, Price: {self.__price}, Quantity: {self.__quantity}") 
+    def set_price(self, new_price):
+        if new_price > 0:
+            self.__price = new_price
+
+product_switch = Product("Nintendo Switch", 400, 89)
+product_switch.get_details()
