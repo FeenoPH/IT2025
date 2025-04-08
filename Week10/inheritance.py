31785bbf66fb59ea6ca5b446a93d4d665e0e01f8
Phoenix Gosper <59917842+FeenoPH@users.noreply.github.com>
2025-04-08 15:44:53 +1000

    Create inheritance.py

---
+++ Week10/inheritance.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def __init__(self, year_manufactured, colour, acceleration_speed):
+        self.year_manufactured = year_manufactured
+        self.colour = colour
+        self.acceleration_speed = acceleration_speed
+    def start_engine():
+        print("The engine is started up")
+    
+class Car(Vehicle):
+    
