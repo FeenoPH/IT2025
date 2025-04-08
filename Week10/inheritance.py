996fe0461df8b29926e5ee9e12476d623018fdaf
Phoenix Gosper <59917842+FeenoPH@users.noreply.github.com>
2025-04-08 16:01:57 +1000

    Update inheritance.py

--- Week10/inheritance.py
+++ Week10/inheritance.py
@@ -1,10 +1,25 @@
 class Vehicle:
-    def __init__(self, year_manufactured, colour, acceleration_speed):
+    def __init__(self, year_manufactured, model, colour):
         self.year_manufactured = year_manufactured
+        self.model = model
         self.colour = colour
-        self.acceleration_speed = acceleration_speed
-    def start_engine():
-        print("The engine is started up")
-    
+    def start_engine(self):
+        print("The engine is starting up")
+
 class Car(Vehicle):
-    
+    def __init__(self, year_manufactured, model, colour):
+        super().__init__(year_manufactured, model, colour)
+    def start_engine(self):
+        print(f"The {self.colour} {self.year_manufactured} {self.model} car is starting its engine")
+
+class Motorcycle(Vehicle):
+    def __init__(self, year_manufactured, model, colour):
+        super().__init__(year_manufactured, model, colour)
+    def start_engine(self):
+        print(f"The {self.colour} {self.year_manufactured} {self.model} motorcycle is starting its engine")
+
+car = Car(1972, "Volkswagen Beetle", "red")
+motorcycle = Motorcycle(2010, "Royal Enfield Classic 350", "purple")
+
+car.start_engine()
+motorcycle.start_engine()
